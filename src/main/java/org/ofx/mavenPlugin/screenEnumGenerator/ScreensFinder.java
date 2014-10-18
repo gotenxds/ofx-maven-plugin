71a9872d59f7062c2ac51edb52510ec97d82164c
David limkys <David.limkys@gmail.com>
2014-10-18 21:40:29 +0300

    Created ScreensFinder - Supplies the FXML dir, and the FXML files.

---
+++ src/main/java/org/ofx/mavenPlugin/screenEnumGenerator/ScreensFinder.java
@@ -0,0 +1,35 @@
+package org.ofx.mavenPlugin.screenEnumGenerator;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public class ScreensFinder
+{
+    public Stream<Path> getScreensFrom(Path porjectPath)
+    {
+        try
+        {
+            return Files.walk(getFxmlDir(porjectPath));
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Path getFxmlDir(Path projectPath)
+    {
+        try
+        {
+            return Files.find(projectPath, 10, (path, basicFileAttributes) -> basicFileAttributes.isDirectory() && path.endsWith("fxml"))
+                        .findAny()
+                        .get();
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException("Could not find the fxml directory.");
+        }
+    }
+}
